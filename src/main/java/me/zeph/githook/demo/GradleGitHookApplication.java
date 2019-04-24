6e0c7579591617d4f25937211e6c34e95c7af580
Benwei Zhu <benweizhu@gmail.com>
2019-04-24 16:59:11 +0800

    verify the pre-commit

---
+++ src/main/java/me/zeph/githook/demo/GradleGitHookApplication.java
@@ -0,0 +1,13 @@
+package me.zeph.githook.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleGitHookApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GradleGitHookApplication.class, args);
+  }
+
+}
